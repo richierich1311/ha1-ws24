e4efcf54f249dc8b80e20f5b7c3287e4ef6bcdff
Richie Hanitzsch <richie.hanitzsch@gmail.com>
2024-10-27 18:35:47 +0100

    zwei rote Tests hinzugefügt

--- app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -100,5 +100,28 @@ class CalculatorTest {
         String actual = calc.readScreen();
         assertEquals(expected, actual);
     }
+
+    @Test
+    @DisplayName("should display Error after pressing 0 and 1 divid x")
+    void testNullBeforeUnaryOperationKey() {
+        Calculator calc = new Calculator();
+        calc.pressDigitKey(0);
+        calc.pressUnaryOperationKey("1/x");
+        String expected = "Error";
+        String actual = calc.readScreen();
+        assertEquals(expected, actual);
+    }
+    @Test
+    @DisplayName("should expect to be zero after multiplying, pressing key and pressing negative key")
+    void testBinaryKeyBeforeNegativeKey() {
+        Calculator calc = new Calculator();
+        calc.pressDigitKey(0);
+        calc.pressBinaryOperationKey("x");
+        calc.pressNegativeKey();
+        calc.pressEqualsKey();
+        String expected = "0";
+        String actual = calc.readScreen();
+        assertEquals(expected, actual);
+    }
 }
 
